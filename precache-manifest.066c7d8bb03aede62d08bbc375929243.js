25628f1ba0e487033f4ac522e86bc8a8700933fe
maciejPaluszkiewicz <maciej.j.paluszkiewicz@gmail.com>
2019-07-14 20:12:13 +0200

    Updates

---
+++ precache-manifest.066c7d8bb03aede62d08bbc375929243.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2bfa96cc3231f323288a98bda95a4791",
+    "url": "/React-FlickrRecentViewer/index.html"
+  },
+  {
+    "revision": "23ad933fd947fc4c8f65",
+    "url": "/React-FlickrRecentViewer/static/css/main.18737a31.chunk.css"
+  },
+  {
+    "revision": "3384ac152b06dab37729",
+    "url": "/React-FlickrRecentViewer/static/js/2.a5d9e200.chunk.js"
+  },
+  {
+    "revision": "23ad933fd947fc4c8f65",
+    "url": "/React-FlickrRecentViewer/static/js/main.c2be91f9.chunk.js"
+  },
+  {
+    "revision": "7dc63aff19692d48d703",
+    "url": "/React-FlickrRecentViewer/static/js/runtime~main.3be6cd54.js"
+  },
+  {
+    "revision": "6123108292e65c28b6fc4dd1bbd62c3f",
+    "url": "/React-FlickrRecentViewer/static/media/under-construction-2408061_640.61231082.png"
+  }
+]);
